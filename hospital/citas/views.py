05df7070267dd2a9c6a806c239d2088da2dbd045
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-12-01 16:37:57 -0500

    fix: no mostrar consultorio en targetas de citas agendadas

--- hospital/citas/views.py
+++ hospital/citas/views.py
@@ -207,6 +207,7 @@ def confirmar_cita(request):
     if request.method == "POST":
         medico_id = request.POST.get("medico_id")
         especialidad_id = request.POST.get("especialidad_id")
+        consultorio_id = request.POST.get("consultorio_id") # obtener asignación de consultorio de forma dinámica
         fecha_str = request.POST.get("fecha") # fecha obtenida del form
         hora_inicio_str = request.POST.get("hora_inicio") # hora inicio obtenida del form
         hora_fin_str = request.POST.get("hora_fin") # hora fin obtenida del form
@@ -224,10 +225,20 @@ def confirmar_cita(request):
         medico = Medico.objects.get(pk=medico_id)
         especialidad = Especialidad.objects.get(pk=especialidad_id)
 
+        # Asignación dinámica para el consultorio interno o externo
+        consultorio = None # por defecto
+        # Si el consultorio_id es None, significa que el médico es externo y no tiene consultorio
+        if consultorio_id:
+            consultorio = Consultorio.objects.get(pk=consultorio_id)
+        
+        # si el medico es interno
+        else:
+            consultorio = medico.consultorio
+
         cita = Cita.objects.create(
             paciente = Usuario.objects.get(id=request.session["usuario_id"]), # Obtiene el usuario de la sesión manual
             medico=medico,
-            consultorio=medico.consultorio,
+            consultorio=consultorio,
             especialidad=especialidad,
             fecha=fecha,
             hora_inicio=hora_inicio,
