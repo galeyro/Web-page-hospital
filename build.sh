9573fa14614e807736fd3460b303e98b33827115
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2026-01-25 17:14:15 -0500

    build: new files for sample data to deploy in render

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -o errexit
 
-pip install -r requirements.txt
+# 1. Build Frontend
+echo "🏗️ Building Frontend..."
+cd frontend
+npm install
+npm run build
+cd ..
+
+# 2. Build Backend
+echo "🐍 Building Backend..."
+pip install -r hospital/requirements.txt
 
 cd hospital
 python manage.py collectstatic --no-input
